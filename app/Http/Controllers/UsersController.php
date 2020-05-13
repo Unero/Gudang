b274e2501a72c6fc60708b40110dffdd5cf97f8f
Unero <20809454+Unero@users.noreply.github.com>
2020-05-13 22:55:41 +0700

    Setup REST Client 🙋‍♂️

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class UsersController extends Controller
+{
+    public function index(){
+        $response = Http::get('http://localhost/Gudang-Backend/API/Users');
+        echo $response;
+    }
+}
