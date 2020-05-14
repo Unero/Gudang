4120cd3344937cdac7eff640d4089f096f0b3645
wildanbash <wildan.semua.akun@gmail.com>
2020-05-15 05:05:23 +0700

    Update: REST Client (Stores)

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -3,10 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class StoresController extends Controller
 {
     public function index(){
-        return view('admin/stores');
+        $request = Http::get('http://localhost/Gudang-Backend/API/Stores');
+        $stores = json_decode($request->body(), true);
+        return view('admin/stores', ['store' => $stores]);
+    }
+
+    public function add(Request $request)
+    {
+        $client = Http::post('http://localhost/Gudang-Backend/API/Stores', [
+            'name' => $request->name,
+            'active' => $request->active
+        ]);
+
+        if ($client->status() == 200) {
+            return redirect('/stores');
+        } else {
+            return redirect('/dashboard');
+        }
+    }
+
+    public function hapus($id)
+    {
+        $client = Http::asForm()->delete('http://localhost/Gudang-Backend/API/Stores', [
+            'id' => $id
+        ]);
+
+        if ($client['status'] == 'success') {
+            return redirect('/stores');
+        } else {
+            return redirect('/dashboard');
+        }
     }
 }
