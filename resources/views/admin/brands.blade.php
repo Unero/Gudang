f0b081ff59526e85bb54d2612c4cf5a65828fcd5
wildanbash <wildan.semua.akun@gmail.com>
2020-05-14 18:35:19 +0700

    Add brand pages

---
+++ resources/views/admin/brands.blade.php
@@ -0,0 +1,106 @@
+@include('layouts.master')
+
+<!-- /.navbar -->
+@include('layouts.header')
+
+<!-- Main Sidebar Container -->
+@include('layouts.sidebar')
+
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Manage Brand</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Brand</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+
+
+    <section class="content">
+        <div class="container-fluid">
+            <!-- COLOR PALETTE -->
+            <div class="card">
+                <div class="card-header">
+
+
+                    <div class="col-6">
+                        <i class="fas fa-copy"></i>
+                        Brand
+                    </div>
+                    <div class="col-6 mt-2">
+                        <a data-toggle="modal" data-target="#addBrandModal" class="btn btn-secondary btn-xs text-white">
+                            <i class="fas fa-plus"></i>
+                            Add Brand
+                        </a>
+                    </div>
+
+
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-bordered" id="datatables">
+                        <thead>
+                            <th style="width: 10%">No</th>
+                            <th style="width: 45%">Brand Name</th>
+                            <th style="width: 25%">Status</th>
+                            <th style="width: 20%">Action</th>
+                        </thead>
+                        <tbody>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@include('layouts.footer')
+
+<div class="modal fade" tabindex="-1" role="dialog" id="addBrandModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Add Brand</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+
+            <form role="form" action="" method="post"
+                id="createBrandForm">
+
+                <div class="modal-body">
+
+                    <div class="form-group">
+                        <label for="brand_name">Brand Name</label>
+                        <input type="text" class="form-control" id="brand_name" name="brand_name"
+                            placeholder="Enter brand name" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="active">Status</label>
+                        <select class="form-control" id="active" name="active">
+                            <option value="1">Active</option>
+                            <option value="2">Inactive</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+
+            </form>
+
+
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
